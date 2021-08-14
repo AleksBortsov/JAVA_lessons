48df11d5213a5d5350afb64b80a345d9ef95d5c4
Oleksandr <bortsov.nuos@gmail.com>
2021-08-14 14:29:44 +0300

    add out folder to git ignore

--- src/EpamLessonsBook/Start.java
+++ src/EpamLessonsBook/Start.java
@@ -1,7 +1,17 @@
 package EpamLessonsBook;
 
+import java.nio.charset.StandardCharsets;
+
 public class Start {
     public static void main(String[] args) {
         System.out.println("Hello starting lessons from Epam book");
+    }
+
+    Start start = new Start(6, 6);
+
+    public Start(int a, int b) {
+        a = 5;
+        b = 5;
+        int c = a*b;
     }
 }
