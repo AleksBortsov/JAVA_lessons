29208c8af499bf158634a5b75272205570309ea9
Oleksandr <bortsov.nuos@gmail.com>
2021-08-15 22:58:38 +0300

    Chapter1OptionA_3

---
+++ src/EpamLessonsBook/Chapter1OptionA_3.java
@@ -0,0 +1,20 @@
+package EpamLessonsBook;
+
+import java.util.Scanner;
+
+public class Chapter1OptionA_3 {
+
+    public static void main(String[] args) {
+        System.out.println("Insert number in new line: ");
+        Scanner scannerNumbers = new Scanner(System.in);
+        Integer numbers = scannerNumbers.nextInt();
+        for (int i = 1; i < numbers + 1; i++)
+            System.out.println(i);
+
+        System.out.println("Insert number in line: ");
+        Scanner scannerLineNumbers = new Scanner(System.in);
+        Integer lineNumbers = scannerLineNumbers.nextInt();
+        for (int n = 1; n < lineNumbers+1; n++)
+            System.out.print(n+" ");
+    }
+}
