bde3239bfccea6da6d5a57b6c068d0b21bbfa573
Oleksandr <bortsov.nuos@gmail.com>
2021-08-15 11:46:47 +0300

    Coin class

--- src/EpamLessonsBook/Start.java
+++ src/EpamLessonsBook/Start.java
@@ -1,6 +1,5 @@
 package EpamLessonsBook;
 
-import java.nio.charset.StandardCharsets;
 
 public class Start {
     public static void main(String[] args) {
@@ -12,6 +11,29 @@ public class Start {
     public Start(int a, int b) {
         a = 5;
         b = 5;
-        int c = a*b;
+        int c = a * b;
+    }
+
+    public double diametr;
+    private double weight;
+
+    public double getDiametr() {
+        return diametr;
+    }
+
+    public void setDiametr(double value) {
+        if (value > 0) {
+            diametr = value;
+        } else {
+            diametr = 0.01; // --> default value if diametr negatibe or 0
+        }
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double value) {
+        weight = value;
     }
 }
