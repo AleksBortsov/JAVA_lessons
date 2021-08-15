85f19efbcb9f88797d058adc5cfd0184402b0b6f
Oleksandr <bortsov.nuos@gmail.com>
2021-08-16 01:10:48 +0300

    Chapter1OptionA_6

---
+++ src/EpamLessonsBook/Chapter1OptionA_6.java
@@ -0,0 +1,42 @@
+package EpamLessonsBook;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Chapter1OptionA_6 {
+
+    public static void main(String[] args) throws ParseException {
+
+        System.out.println("Insert developer`s last name: ");
+        Scanner nameScanner = new Scanner(System.in);
+
+        String nameInsert = nameScanner.next();
+
+
+        System.out.println("Insert date of started job in format: dd/MM/yyyy");
+        Scanner scannerStartDate = new Scanner(System.in);
+        String startDate = scannerStartDate.next();
+        //SimpleDateFormat simpleDateFormat=new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("Insert time of started job in format: HH:mm");
+        Scanner scannerStartTime = new Scanner(System.in);
+        String startTime = scannerStartTime.next();
+
+        System.out.println("Insert date of finished job in format: dd/MM/yyyy");
+        Scanner scannerStopDate = new Scanner(System.in);
+        String stopDate = scannerStopDate.next();
+
+        System.out.println("Insert time of finished job in format: HH:mm");
+        Scanner scannerStopTime = new Scanner(System.in);
+        String stopTime = scannerStopTime.next();
+
+        System.out.println("Hello dear developer, Mr." + nameInsert);
+        System.out.println("You job started at " + startDate + ", " + "in " + startTime);
+        System.out.println("You job finished at " + stopDate + ", " + "in " + stopTime);
+
+    }
+}
