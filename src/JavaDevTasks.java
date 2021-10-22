02855aabfb43d32252170f0bc890ffef8a752699
Oleksandr <bortsov.nuos@gmail.com>
2021-10-22 14:03:28 +0300

    check happy number from array

--- src/JavaDevTasks.java
+++ src/JavaDevTasks.java
@@ -1,4 +1,3 @@
-import java.nio.charset.IllegalCharsetNameException;
 import java.util.*;
 
 public class JavaDevTasks {
@@ -23,7 +22,7 @@ public class JavaDevTasks {
         return false;
     }
 
-    private static boolean dublicateInSet(int data[]) {
+    private static boolean dublicateInSet(int[] data) {
         Set<Integer> inSet = new HashSet<>();
         for (int i = 0; i < data.length; i++) {
             if (!inSet.add(data[i]))
@@ -36,7 +35,6 @@ public class JavaDevTasks {
         //1. if min<value - return min  5    7         6
         //2. if value>max - return max
         //3. if min<value<max - return value
-//        return Math.min(min, Math.min(max, value));
         return Math.min(max, Math.max(min, value));
     }
 
@@ -59,62 +57,44 @@ public class JavaDevTasks {
         return n;
     }
 
-//    private static int fibo(int n) {
-//        if (n > 2) {
-//            if (map.containsKey(n - 1)) {
-//                int k = map.get(n - 1) + map.get(n - 2);
-//                map.put(n, k);
-//                return k;
-//            } else {
-//                int k = fibo(n - 1) + fibo(n - 2);
-//                map.put(n, k);
-//                System.out.println("n= " + n + ", fibonaci= " + map.get(n));
-//                return k;
-//            }
-//        } else {
-//            if (!map.containsKey(n)) {
-//                map.put(n, n);
-//                System.out.println("n= " + n + ", fibonaci= " + map.get(n));
-//            }
-//        }
-//        return n;
-//    }
+    private static void happyNumbersFromArray(int[] data) {
 
-//    private static int fibonacci_rev3(int n) {
-//        if (n > 2) {
-//            if (!map.containsKey(n - 1)) {
-//                int k = fibonacci_rev3(n - 1);
-//                map.put(n - 1, k);
-//                return n;
-//            }
-//
-//            if (!map.containsKey(n - 2)) {
-//                int m = fibonacci_rev3(n - 2);
-//                map.put(n - 2, m);
-//                return m;
-//            }
-//
-//            System.out.println("n= " + n + ", fibonaci= " + map.get(n - 2) + map.get(n - 1));
-//
-//
-//        } else {
-//            map.put(n, n);
-//            map.put(n + 1, n + 1);
-//            System.out.println("n= " + n + ", fibonaci= " + map.get(n));
-//            System.out.println("n= " + (n + 1) + ", fibonaci= " + map.get(n + 1));
-//        }
-//        return n;
-//    }
+    }
+
+    private static int isHappyNumber(int number) {
+        int rem = 0, sum = 0;
+// calculate the sum of squares of digits
+        while (number > 0) {
+            rem = number % 10;
+            sum = sum + (rem * rem);
+            number = number / 10;
+        }
+        return sum;
+    }
+
+    private static void checkHappyNumber(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+            int result = data[i];
+            while (result != 1 && result != 4) {
+                result = isHappyNumber(result);
+            }
+            if (result == 1) {
+                System.out.println("Happy number = " + data[i]);
+            } else if (result == 4) {
+                System.out.println("NOT happy number = " + data[i]);
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
-        int data[] = {-7, -6, -5, -4, -3, -2, -2};
+        int data[] = {1, 2, 82, 13, 14, 15, 23};
 //        System.out.println("MedianaCalculation: " + medianaCalculation(data));
 //        System.out.println("Dublicate in array: " + dublicate(data));
 //        System.out.println("Dublicate in Set: " + dublicateInSet(data));
 //        System.out.println("in Range: " + inRange(5, 7, 6));
-        fibonaci(10);
-//        fibo(10);
+//        fibonaci(10);
+        checkHappyNumber(data);
 
-//        fibonacci_rev3(6);
     }
 }
